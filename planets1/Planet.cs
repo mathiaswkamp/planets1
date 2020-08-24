a0d12d1b094e0d630331d2a45a3e11b6725bac75
mathias kamp <mathias.kamp@hotmail.com>
2020-08-24 22:48:06 +0200

    Add project files.

---
+++ planets1/Planet.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+
+namespace planets1
+{
+    class Planet
+    {
+        static List<Planet> planets = new List<Planet>();      //creates a static list of planet class
+
+
+        private string name;                                   //property of name
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double mass;                                   //property of mass
+
+        public double Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+
+        private double diameter;                              //property of diameter
+
+        public double Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; }
+        }
+
+        private double dencity;                               //property of dencity
+
+        public double Dencity
+        {
+            get { return dencity; }
+            set { dencity = value; }
+        }
+
+        private double gravity;                              // property of gravity
+
+        public double Gravity
+        {
+            get { return gravity; }
+            set { gravity = value; }
+        }
+
+        private double rotationperiod;                      //property of rotation period
+
+        public double RotationPeriod                        
+        {
+            get { return rotationperiod; }
+            set { rotationperiod = value; }
+        }
+
+        private double lengthOfDay;                         //property of length of day
+
+        public double LengthOfDay
+        {
+            get { return lengthOfDay; }
+            set { lengthOfDay = value; }
+        }
+
+        private double distanceFromSun;                     //property of distance from sun
+
+        public double DistanceFromSun
+        {
+            get { return distanceFromSun; }
+            set { distanceFromSun = value; }
+        }
+
+        private double orbitalPeriod;                       //property orbital period
+
+        public double OrbitalPeriod
+        {
+            get { return orbitalPeriod; }
+            set { orbitalPeriod = value; }
+        }
+
+        private double orbitalVelocity;                     //property of orbital velocity
+
+        public double OrbitalVelocity
+        {
+            get { return orbitalVelocity; }
+            set { orbitalVelocity = value; }
+        }
+
+        private double meanTemperature;
+
+        public double MeanTemperature                       //property of mean temperature
+        {
+            get { return meanTemperature; }
+            set { meanTemperature = value; }
+        }
+
+        private double numberofMoons;                       // property of number of moons
+
+        public double NumberOfMoons             
+        {
+            get { return numberofMoons; }
+            set { numberofMoons = value; }
+        }
+
+        private string ringSystem;                          // property of ring system
+
+        public string RingSysytem
+        {
+            get { return ringSystem; }
+            set { ringSystem = value; }
+        }
+
+        internal static List<Planet> Planets { get => planets; set => planets = value; } // internal static property of planets allows me to create a list
+
+
+        // here is my constructor taking all values with it upon creation of a planet item
+
+        public Planet(string name, double mass, double diameter, double dencity, double gravity, double rotationperiod, double lengthofday, double distancefromsun, double orbitalperiod, double orbitalvelocity, double meantemperature, double numberofmoons, string ringsystem)
+        {
+            this.name = name;
+            this.mass = mass;
+            this.diameter = diameter;
+            this.dencity = dencity;
+            this.gravity = gravity;
+            this.rotationperiod = rotationperiod;
+            this.lengthOfDay = lengthofday;
+            this.distanceFromSun = distancefromsun;
+            this.orbitalPeriod = orbitalperiod;
+            this.orbitalVelocity = orbitalvelocity;
+            this.meanTemperature = meantemperature;
+            this.numberofMoons = numberofmoons;
+            this.ringSystem = ringsystem;
+
+            planets.Add(this);
+
+        }
+
+
+        
+    }
+}
---
+++ planets1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planets1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Mercury = new Planet("Mercury", 0.330, 4879, 5427, 3.7, 1407.6, 4222.6, 57.9, 88.0, 47.4, 167, 0, "No");     // create a planet object of the class planet 
+            Planet Venus = new Planet("Venus", 4.87, 12104, 5243, 8.9, -5832.5, 2802.0, 108.2, 224.7, 35.0, 464, 0, "No");      // create a planet object of the class planet 
+            Planet Earth = new Planet("Earth", 5.97, 12756, 5514, 9.8, 23.9, 24.0, 149.6, 365.2, 29.8, 15, 1, "No");            // create a planet object of the class planet
+            Planet Mars = new Planet("Mars", 0.642, 6792, 3933, 3.7, 24.6, 24.7, 227.9, 687.0, 24.1, -65, 2, "No");             // create a planet object of the class planet
+            Planet Jupiter = new Planet("Jupiter", 1898, 142984, 1326, 23.1, 9.9, 9.9, 778.6, 4331, 13.1, -110, 67, "Yes");     // create a planet object of the class planet
+            Planet Saturn = new Planet("Saturn", 568, 120536, 687, 9.0, 10.7, 10.7, 1433.5, 10.747, 9.7, -140, 62, "Yes");      // create a planet object of the class planet
+            Planet Uranus = new Planet("Uranus", 86.8, 51118, 1271, 8.7, -17.2, 17.2, 2872.5, 30.589, 6.8, -195, 27, "Yes");    // create a planet object of the class planet    
+            Planet Neptune = new Planet("Neptune", 102, 49528, 1638, 11.0, 16.1, 16.1, 4495.1, 59.8, 5.4, -200, 14, "Yes");     // create a planet object of the class planet
+            Planet Pluto = new Planet("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90.56, 4.7, -255, 5, "No");      // create a planet object of the class planet
+
+            List<Planet> Planets = new List<Planet>();                                                          // creates a list 
+            Planets =  Planet.Planets;                                                                          // puts the planets into the list
+
+            Planets.RemoveAt(8);                                                                           // removes index 8 which is pluto
+            Planets.Add(Pluto);                                                                                 // adds pluto back to the list
+
+            Planets.ForEach(o => Console.WriteLine($"{o.Name} : mass : {o.Mass}(1024kg) diameter : {o.Diameter}(km) dencity : {o.Dencity}(kg/m3) gravity {o.Gravity}(m/s2) rotation period :{o.RotationPeriod}hrs length of day : {o.LengthOfDay}hrs distance from sun : {o.DistanceFromSun}(106km) orbital period : {o.OrbitalPeriod}days orbital velocity : {o.OrbitalVelocity}(km/s) mean temperature : {o.MeanTemperature}celcius  number of moons : {o.NumberOfMoons} ring system : {o.RingSysytem}"));    //prints out the list of Planets
+
+            int count = Planets.Count();                                                                        // creates an int that counts how many planets that are in the list
+            Console.WriteLine($"There is : {count} Planets");
+
+            Console.WriteLine("press any key to see list of planets with a Mean Temperature below 0");
+            Console.ReadKey();
+            Console.Clear();
+
+            List<Planet> temperatureBelowZero = new List<Planet>();                                             // creates a new list 
+
+            temperatureBelowZero.Add(Mars);                                                                     // adds the following planets to it
+            temperatureBelowZero.Add(Jupiter);
+            temperatureBelowZero.Add(Saturn);
+            temperatureBelowZero.Add(Uranus);
+            temperatureBelowZero.Add(Neptune);
+            temperatureBelowZero.Add(Pluto);
+
+            temperatureBelowZero.ForEach(o => Console.WriteLine($"{o.Name} : mass : {o.Mass}(1024kg) diameter : {o.Diameter}(km) dencity : {o.Dencity}(kg/m3) gravity {o.Gravity}(m/s2) rotation period :{o.RotationPeriod}hrs length of day : {o.LengthOfDay}hrs distance from sun : {o.DistanceFromSun}(106km) orbital period : {o.OrbitalPeriod}days orbital velocity : {o.OrbitalVelocity}(km/s) mean temperature : {o.MeanTemperature}celcius  number of moons : {o.NumberOfMoons} ring system : {o.RingSysytem}"));
+
+
+            Console.WriteLine("press any key to see list of planets with a diameter between 10000km and 50000km");
+            Console.ReadKey();
+            Console.Clear();
+            List<Planet> diameterBetween10kto50k = new List<Planet>();                                           // creates a new list
+            diameterBetween10kto50k.Add(Venus);                                                                  // add plantes to list
+            diameterBetween10kto50k.Add(Earth);
+
+            diameterBetween10kto50k.ForEach(o => Console.WriteLine($"{o.Name} : mass : {o.Mass}(1024kg) diameter : {o.Diameter}(km) dencity : {o.Dencity}(kg/m3) gravity {o.Gravity}(m/s2) rotation period :{o.RotationPeriod}hrs length of day : {o.LengthOfDay}hrs distance from sun : {o.DistanceFromSun}(106km) orbital period : {o.OrbitalPeriod}days orbital velocity : {o.OrbitalVelocity}(km/s) mean temperature : {o.MeanTemperature}celcius  number of moons : {o.NumberOfMoons} ring system : {o.RingSysytem}"));
+
+
+            Console.WriteLine("press any key to remove all planets from the list ");
+            Console.Clear();
+            Console.ReadKey();
+
+            Planets.Clear();                                                                                     // clear all 3 lists of planets
+            temperatureBelowZero.Clear();
+            diameterBetween10kto50k.Clear();
+
+            Console.WriteLine("the 3 lists of planets has been cleared ");
+
+            diameterBetween10kto50k.ForEach(o => Console.WriteLine($"{o.Name} : mass : {o.Mass}(1024kg) diameter : {o.Diameter}(km) dencity : {o.Dencity}(kg/m3) gravity {o.Gravity}(m/s2) rotation period :{o.RotationPeriod}hrs length of day : {o.LengthOfDay}hrs distance from sun : {o.DistanceFromSun}(106km) orbital period : {o.OrbitalPeriod}days orbital velocity : {o.OrbitalVelocity}(km/s) mean temperature : {o.MeanTemperature}celcius  number of moons : {o.NumberOfMoons} ring system : {o.RingSysytem}"));   // this line is to check if the lists is cleared
+        }
+    }
+}
